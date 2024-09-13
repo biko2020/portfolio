c41bbc7b4c7b4fdbdd3ac49989bfe1c9205733bf
biko2020 <aitoufkirbrahimab@gmail.com>
2024-09-13 11:24:40 +0100

    add while and for loop in python

---
+++ test.py
@@ -0,0 +1,10 @@
+i=1
+while i*i < 100:
+     print("Square of", i, "is", i*i )
+     i = i + 1
+print("The squares below 100.")
+
+# t=0
+# for i in [0,1,2,3,4,5,6,7,8,9]:
+#     t = t + i
+# print("The sum is", t)
